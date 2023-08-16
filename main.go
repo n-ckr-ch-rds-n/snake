cd3a62f8ccdd177d58fb859a2f7a089dcf1eeeec
Nick Richardson <nicholasjohnrichardson@gmail.com>
2023-08-16 17:26:52 +0100

    Print horizontal borders

--- main.go
+++ main.go
@@ -22,14 +22,23 @@ type Apple struct {
 }
 
 var screenWidth, screenHeight int
+var Screen tcell.Screen
+
 var snake *Snake
 var apple *Apple
 
 const FRAME_WIDTH = 80
 const FRAME_HEIGHT = 15
-const FRAME_BORDER_THICKNESS = 1
 const SNAKE_SYMBOL = 0x2588
 const APPLE_SYMBOL = 0x25CF
+
+const FRAME_BORDER_THICKNESS = 1
+const FRAME_BORDER_VERTICAL = '║'
+const FRAME_BORDER_HORIZONTAL = '═'
+const FRAME_BORDER_TOP_LEFT = '╔'
+const FRAME_BORDER_TOP_RIGHT = '╗'
+const FRAME_BORDER_BOTTOM_RIGHT = '╝'
+const FRAME_BORDER_BOTTOM_LEFT = '╚'
 
 func main() {
 	initScreen()
@@ -38,7 +47,7 @@ func main() {
 
 func initScreen() {
 	var err error
-	Screen, err := tcell.NewScreen()
+	Screen, err = tcell.NewScreen()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
@@ -63,7 +72,7 @@ func initGameObjects() {
 		rowVelocity:    1,
 		symbol:         SNAKE_SYMBOL,
 	}
-	apple := &Apple{
+	apple = &Apple{
 		point: getInitialAppleCoordinates(),
 		symbol: APPLE_SYMBOL,
 	}
@@ -116,4 +125,62 @@ func getBoundaries() (int, int, int, int) {
 
 func getFrameOrigin() (int, int) {
 	return (screenWidth-FRAME_WIDTH)/2 - FRAME_BORDER_THICKNESS, (screenHeight-FRAME_HEIGHT)/2 - FRAME_BORDER_THICKNESS
+}
+
+func displayFrame() {
+	frameOriginX, frameOriginY := getFrameOrigin()
+	printUnfilledRectangle(
+		frameOriginX,
+		frameOriginY,
+		FRAME_WIDTH,
+		FRAME_HEIGHT,
+		FRAME_BORDER_THICKNESS,
+		FRAME_BORDER_HORIZONTAL,
+		FRAME_BORDER_VERTICAL,
+		FRAME_BORDER_TOP_LEFT,
+		FRAME_BORDER_TOP_RIGHT,
+		FRAME_BORDER_BOTTOM_RIGHT,
+		FRAME_BORDER_BOTTOM_LEFT,
+	)
+	Screen.Show()
+}
+
+func printUnfilledRectangle(
+	xOrigin int,
+	yOrigin int,
+	width int,
+	height int,
+	borderThickness int,
+	horizontalOutline rune,
+	verticalOutline rune,
+	topLeftOutline rune,
+	topRightOutline rune,
+	bottomRightOutline rune,
+	bottomLeftOutline rune,
+) {
+	var upperBorder, lowerBorder rune
+	verticalBorder := verticalOutline
+	for i := 0; i < width; i++ {
+		if i == 0 {
+			upperBorder = topLeftOutline
+			lowerBorder = bottomLeftOutline
+		} else if i == width - 1 {
+			upperBorder = topRightOutline
+			lowerBorder = bottomRightOutline
+		} else {
+			upperBorder = horizontalOutline
+			lowerBorder = horizontalOutline
+		}
+		printToFrame(xOrigin+i, yOrigin, borderThickness, borderThickness, tcell.StyleDefault, upperBorder)
+		printToFrame(xOrigin+i, yOrigin+height-1, borderThickness, borderThickness, tcell.StyleDefault, lowerBorder)
+	}
+	
+}
+
+func printToFrame(x, y, w, h int, style tcell.Style, char rune) {
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j ++ {
+			Screen.SetContent(x+i, y+j, char, nil, style)
+		}
+	}
 }
